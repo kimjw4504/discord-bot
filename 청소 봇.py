3423ef6d9684b07ee5559f314f96fe3f85fac179
kimjw4504 <110798025+kimjw4504@users.noreply.github.com>
2022-08-08 11:36:22 +0900

    Add files via upload

---
+++ 청소 봇.py
@@ -0,0 +1,28 @@
+# 파이썬의 기본 내장 함수가 아닌 다른 함수 혹은 다른 기능이 필요할 때 사용함
+import discord, asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): # 봇이 실행되면 한 번 실행됨
+    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("!청소 명령어 사용중"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith ("!청소"):
+        if message.author.guild_permissions.administrator:
+            amount = message.content[4:]
+            await message.delete()
+            await message.channel.purge(limit=int(amount))
+
+            embed = discord.Embed(title="메시지 삭제 알림", description="최근 디스코드 채팅 {}개가\n관리자 {}님의 요청으로 인해 정상 삭제 조치 되었습니다".format(amount, message.author), color=0xff4343)
+            embed.set_footer(text="Bot Made by. 구늘이#0465", icon_url="https://discordapp.com/channels/691615852620939274/703908401381376000/711859989177958410")
+            await message.channel.send(embed=embed)
+        
+        else:
+            await message.delete()
+            await message.channel.send("{}, 당신은 명령어를 사용할 수 있는 권한이 없습니다".format(message.author.mention))
+
+# 봇을 실행시키기 위한 토큰을 작성해주는 곳
+client.run('MTAwNTY4MDQyNzYwMTc2NDUxNA.GgdjBS.Id0SNrW2xofd5BPb6soH__Ilxkd9jJysr2MTps')
